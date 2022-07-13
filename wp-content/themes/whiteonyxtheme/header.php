48b3b98e3692b8154d11ab18bf1300aab67defaf
Alex Pozdnii <49003245+alexpozdnii97@users.noreply.github.com>
2022-07-13 23:57:07 +0300

    gg

--- wp-content/themes/whiteonyxtheme/header.php
+++ wp-content/themes/whiteonyxtheme/header.php
@@ -18,7 +18,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
-
+		<meta name="google-site-verification" content="YsRnhDYaiNctxT2l9Nt5FvIDEnqYIq83HJmFqkl_dZI" />
 		<?php wp_head(); ?>
 	</head>
 
