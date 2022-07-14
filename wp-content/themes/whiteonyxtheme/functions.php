38b52c5cff56d4570b0630c2085cfda01dffc819
Alex Pozdnii <49003245+alexpozdnii97@users.noreply.github.com>
2022-07-14 15:08:19 +0300

    all styles

--- wp-content/themes/whiteonyxtheme/functions.php
+++ wp-content/themes/whiteonyxtheme/functions.php
@@ -147,7 +147,8 @@ function studio_main_theme_scripts() {
 	wp_style_add_data( 'studio_main_theme-style', 'rtl', 'replace' );
 
 
-	
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() .'/assets/css/bootstrap.min.css' );
+	wp_enqueue_style( 'fontawesone-style', get_template_directory_uri() .'/assets/css/fontawesome.css' );
 	wp_enqueue_style( 'magnific-style', get_template_directory_uri() .'/assets/css/magnific-popup.css' );
 	wp_enqueue_style( 'nice-select-style', get_template_directory_uri() .'/assets/css/nice-select.css' );
 
@@ -176,8 +177,7 @@ add_action( 'wp_enqueue_scripts', 'studio_main_theme_scripts' );
  */
 
 function prefix_add_footer_styles() {
-  wp_enqueue_style( 'fontawesone-style', get_template_directory_uri() .'/assets/css/fontawesome.css' );
-	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() .'/assets/css/bootstrap.min.css' );
+   
 };
 add_action( 'get_footer', 'prefix_add_footer_styles' );
 
