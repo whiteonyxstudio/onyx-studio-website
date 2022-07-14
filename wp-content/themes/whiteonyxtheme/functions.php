b055f055c18ee9f35040785262619dce2d1ce735
Alex Pozdnii <49003245+alexpozdnii97@users.noreply.github.com>
2022-07-14 15:37:02 +0300

    gg

--- wp-content/themes/whiteonyxtheme/functions.php
+++ wp-content/themes/whiteonyxtheme/functions.php
@@ -149,7 +149,9 @@ function studio_main_theme_scripts() {
 
 
 
-	
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() .'/assets/css/bootstrap.min.css' );
+	wp_enqueue_style( 'magnific-style', get_template_directory_uri() .'/assets/css/magnific-popup.css' );
+	wp_enqueue_style( 'nice-select-style', get_template_directory_uri() .'/assets/css/nice-select.css' );
 
 
 	wp_enqueue_style( 'slick-style', get_template_directory_uri() .'/assets/css/slick.css', rand(111,9999), 'all' );
@@ -176,10 +178,7 @@ add_action( 'wp_enqueue_scripts', 'studio_main_theme_scripts' );
  */
 
 function prefix_add_footer_styles() {
-	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() .'/assets/css/bootstrap.min.css' );
-  wp_enqueue_style( 'fontawesone-style', get_template_directory_uri() .'/assets/css/fontawesome.css' );
-	wp_enqueue_style( 'magnific-style', get_template_directory_uri() .'/assets/css/magnific-popup.css' );
-	wp_enqueue_style( 'nice-select-style', get_template_directory_uri() .'/assets/css/nice-select.css' );
+	wp_enqueue_style( 'fontawesone-style', get_template_directory_uri() .'/assets/css/fontawesome.css' );
 };
 add_action( 'get_footer', 'prefix_add_footer_styles' );
 
