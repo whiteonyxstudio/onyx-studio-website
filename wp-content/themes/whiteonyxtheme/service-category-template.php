e6806d260b815776961bab1787563afe667dfe3a
Alex Pozdnii <49003245+alexpozdnii97@users.noreply.github.com>
2022-07-16 22:39:12 +0300

    removed html links

--- wp-content/themes/whiteonyxtheme/service-category-template.php
+++ wp-content/themes/whiteonyxtheme/service-category-template.php
@@ -25,7 +25,7 @@
 
 							<div class="col-12 col-md-6 col-lg-4 order-3 order-md-2">
 								<div data-animation="fadeInUp2" data-delay=".6s">
-									<a class="btn btn_white" href="contact.html">Start a project</a>
+									<a class="btn btn_white" href="<?php the_field('s25_agency_contact_page_link', 'option'); ?>">Start a project</a>
 								</div>
 							</div>
 
@@ -228,7 +228,7 @@
 																<div class="blog_col blog_col_1">
 																	<?php the_sub_field('service_details_content'); ?>
 																	<div data-animation="fadeInUp2" data-delay=".6s">
-																		<a class="btn btn_white" href="contact.html">Start a project</a>
+																		<a class="btn btn_white" href="<?php the_field('s25_agency_start_project_link', 'option'); ?>">Start a project</a>
 																	</div>
 																</div>
 															</div>
@@ -357,7 +357,7 @@
 													<span class="text_effect_wrap3 text-break">Any enquiries? </span>
 												</span>
 											</span>
-											<a href="contact.html">
+											<a href="<?php the_field('s25_agency_contact_page_link', 'option'); ?>">
 												<span class="text_effect_wrap1">
 													<span class="text_effect_wrap2">
 														<span class="text_effect_wrap3 text-break">Get in touch now!</span>
@@ -514,7 +514,7 @@
 													<span class="text_effect_wrap3 text-break">Any enquiries? </span>
 												</span>
 											</span>
-											<a href="contact.html">
+											<a href="<?php the_field('s25_agency_contact_page_link', 'option'); ?>">
 												<span class="text_effect_wrap1">
 													<span class="text_effect_wrap2">
 														<span class="text_effect_wrap3 text-break">Get in touch now!</span>
@@ -615,7 +615,7 @@
 								<div class="col-12 col-md-6">
 									<div class="extra_seo_banner">
 										<h3 class="extra_seo_header">Bespoke web development of websites in London</h3>
-										<a class="btn btn_white" href="contact.html">Discuss a Project</a>
+										<a class="btn btn_white" href="<?php the_field('s25_agency_start_project_link', 'option'); ?>">Discuss a Project</a>
 									</div>
 								</div>
 								<div class="col-12 col-md-6">
