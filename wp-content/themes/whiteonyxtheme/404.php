c7fe26de069adae6625c469b922a936dfea61787
Alex Pozdnii <49003245+alexpozdnii97@users.noreply.github.com>
2022-07-16 22:50:54 +0300

    added 404 page

--- wp-content/themes/whiteonyxtheme/404.php
+++ wp-content/themes/whiteonyxtheme/404.php
@@ -1,60 +1,24 @@
 <?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package studio_main_theme
- */
 
 get_header();
 ?>
-
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'studio_main_theme' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'studio_main_theme' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'studio_main_theme' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$studio_main_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'studio_main_theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$studio_main_theme_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-
+			<main>
+				<section class="error_section section_space  text-center">
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-lg-5 col-md-6">
+								<div class="error_content">
+									<h2>404</h2>
+									<h3>Oops! The page you are looking for does not exist. It might have been moved or deleted.</h3>
+									<p></p>
+									<div class="loadmore_btn_wrap text-center">
+										<a class="btn btn_white text-uppercase" href="<?php echo get_home_url(); ?>">Back to home</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
 <?php
 get_footer();
