a981c1f7c397834d5c94f5fb97ec9a14c093df24
Alex Pozdnii <49003245+alexpozdnii97@users.noreply.github.com>
2022-09-06 15:13:04 +0300

    gg

--- wp-content/themes/whiteonyxtheme/service-category-template.php
+++ wp-content/themes/whiteonyxtheme/service-category-template.php
@@ -7,6 +7,26 @@
 
 				<!-- breadcrumb_section - start
 				================================================== -->
+
+				<section class="service-category-hero">
+					<div class="container custom-flex">
+						<div class="row " >
+							<div class="col-xxl-10 ">
+								<h1><?php the_field('s21_hero_title'); ?> </h1>
+								<p><?php the_field('s21_hero_description'); ?></p>
+								<a class="btn btn_border border_dark text-uppercase" href="<?php the_field('s25_agency_start_project_link', 'option'); ?>">Start a project</a>
+								<a class="btn_text btn_text_white" href="#target_section">
+									<span>Explore more!</span> 
+									<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M10 0V7.50063H9.16833V1.42131L0.587991 10L0 9.41015L8.57868 0.833125H2.49418V0H10Z" fill="white"></path>
+									</svg>
+								</a>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
 				<section class="breadcrumb_section service-main lime_bg ">
 					<div class="container">
 						<div class="row">
